60dc3b83b21f26d604018f6a96a751ffe7eb4bf0
masaki murata <wiz.muramasa.111@gmail.com>
2026-06-10 16:43:57 +0900

    星座データの表示機能を追加

--- starguide/lib/ConstellationList.dart
+++ starguide/lib/ConstellationList.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'data/constellation_data.dart';
 
 class ConstellationList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("星座一覧")),
-      body: Center(child: Text("Constellation List")),
+      body: ListView.builder(
+        itemCount: constellations.length,
+        itemBuilder: (context, index) {
+          final constellation = constellations[index];
+
+          return ListTile(
+            title: Text(constellation.name),
+            subtitle: Text(constellation.description),
+          );
+        },
+      ),
     );
   }
 }
